8941cc3d7e620df07bab1cf342821acf0def4b21
Tran Quang Thai <thaidc24v7k026@vlvh.ctu.edu.vn>
2026-04-13 15:56:07 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactRouter = require('./app/routes/contact.route');
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to contact book application.' });
 });
+
+app.use('/api/contacts', contactRouter);
 
 module.exports = app;
